24d2d7607d3cc984a573b14cb2de5473a22a9697
savan1304 <savanparmar1304@gmail.com>
2024-06-16 16:17:52 -0700

    Assignment 3

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,7 +1,43 @@
+// export default function Home() {
+//   return (
+//     <div className="home">
+//       <h1>Assignment 3</h1>
+//     </div>
+//   );
+// }
+
+
+
+import "../css/Home.css";
+
+import { useAuth0 } from "@auth0/auth0-react";
+import { useNavigate } from "react-router-dom";
+
+
 export default function Home() {
+  const navigate = useNavigate();
+  const { isAuthenticated, loginWithRedirect } = useAuth0();
+  const signUp = () => loginWithRedirect({ screen_hint: "signup" });
+
   return (
     <div className="home">
-      <h1>Assignment 3</h1>
+      <h1>Welcome</h1>
+      <div>
+        {!isAuthenticated ? (
+          <button className="btn-primary" onClick={loginWithRedirect}>
+            Login
+          </button>
+        ) : (
+          <button className="btn-primary" onClick={() => navigate("/app")}>
+            Enter App
+          </button>
+        )}
+      </div>
+      <div>
+        <button className="btn-secondary" onClick={signUp}>
+          Create Account
+        </button>
+      </div>
     </div>
   );
 }
