eb5eb22d9a37450ea6878156759a536a2bdce9a6
savan1304 <savanparmar1304@gmail.com>
2024-06-27 15:57:52 -0700

    login restriction

--- client/src/components/home/LoginSignup.jsx
+++ client/src/components/home/LoginSignup.jsx
@@ -3,15 +3,13 @@ import { Auth0Provider, useAuth0 } from '@auth0/auth0-react';
 
 
 const LoginSignup = () => {
-  const { logout, user, isAuthenticated } = useAuth0();
+  const { logout, user, isAuthenticated, loginWithRedirect } = useAuth0();
   console.log("inside LoginSignUp.jsx")
   const redirect_uri= `${window.location.origin}/Festival/:id`
 
 
   const handleLogin = () => {
-    <Auth0Provider>
     loginWithRedirect({redirect_uri});
-    </Auth0Provider>
   };
 
   return (
