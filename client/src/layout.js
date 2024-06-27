2d64976b40e9a03b2330b1750e71480d1173d316
savan1304 <savanparmar1304@gmail.com>
2024-06-27 15:40:01 -0700

    Assignment 3

--- client/src/layout.js
+++ client/src/layout.js
@@ -4,7 +4,7 @@ import Footer from './components/Footer';
 import './css/Layout.css'
 
 const Layout = ({ children }) => {
-    return ( 
+    return (
         <div>
             <h1>Vancouver Fest</h1>
             <Navbar />
